9e7fbc03b10684097c0794819257d0c2d052cedf
ToskeHilsi <dudelamoure@gmail.com>
2026-04-14 11:56:25 -0700

    delete old ai code

--- deltarune_clone/enemy.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Enemy:
-    def __init__(self):
-        self.image = pygame.image.load("assets/enemy.png")
-        self.image = pygame.transform.scale(self.image, (128, 128))
-
-        self.hp = 50
-        self.mercy = 0
--- deltarune_clone/inventory.py
+++
@@ -1,8 +0,0 @@
-class Item:
-    def __init__(self, name, heal=0, attack=0):
-        self.name = name
-        self.heal = heal
-        self.attack = attack
-
-def use_item(player, item):
-    player.hp = min(player.max_hp, player.hp + item.heal)
